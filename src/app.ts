f4309f69064366a7bdddba71a477b9b1bab5187e
Alex McLean <alex.mclean@thomsonreuters.com>
2018-07-03 14:52:30 -0400

    End of typeof and Type Guards

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
-
-interface TrackStates {
-    current: string;
-    next: string;
+function foo(bar: string | number) {
+    if (typeof bar === 'string') {
+        //string -- typescript will give me itellisense here for string typed bar
+        return bar.toUpperCase();
+    }
+    if (typeof bar === 'number') {
+        return bar.toFixed(2);
+    }
 }
 
-const item: Record<keyof TrackStates, string> = {
-    current: 'js02js9',
-    next: '8nlkksjsk'
-};
+class Song {
+    constructor(public title: string, public duration: string | number) { }
+}
 
-let dictionary: Record<string, TrackStates> = {};
-//Numbers are coerced to String
-dictionary[0] = item;
+function getSongDuration(item: Song) {
+    if (typeof item.duration === 'string') {
+        return item.duration;
+    }
+    const { duration } = item;
+    const minutes = Math.floor(duration / 60000);
+    const seconds = (duration / 1000) % 60;
+    return `${(minutes.toString().length === 1) ? '0' + minutes : minutes}:${seconds}`;
+}
+
+const songDurationFromString = getSongDuration(new Song('Wonderful Wonderful', '05:31'));
+console.log(songDurationFromString);
+const songDurationFromNumber = getSongDuration(new Song('Wonderful Wonderful', 330000));
+console.log(songDurationFromNumber);
