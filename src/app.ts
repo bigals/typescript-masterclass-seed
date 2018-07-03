a311ae28d775fc2f88309bc208da0ea9547ad9ef
Alex McLean <alex.mclean@thomsonreuters.com>
2018-07-03 11:44:08 -0400

    End of keyOf Generics and Lookup Types

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,13 @@ type PersonKeys = keyof Person;
 
 type PersonTypes = Person[PersonKeys];
 
+//K is a subtype of T (i.e. looking at function call, name property must exist on person)
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+const personName = getProperty(person, 'name');
+
 const anotherPerson: Person = {
     name: 'John',
     age: 30
