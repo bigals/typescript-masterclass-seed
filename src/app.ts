298e23ef8fc7828f7e4ad56844acc21d577e2a71
Alex McLean <alex.mclean@thomsonreuters.com>
2018-07-03 14:01:45 -0400

    end of pick mapped type

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,16 @@
 interface Person {
     name: string;
-    age?: number;
+    age: number;
+    address: {}
 }
 
-//Equivalent to built in Required<>
-type MyRequired<T> = {
-    [P in keyof T]-?: T[P] //removes the optional modifer on every key
-    //could do -readonly to remove readonly properties
+//Works the same as builtin Pick type
+type MyPick<T, K extends keyof T> = {
+    [P in K]: T[P]
 };
 
-function printAge(person: MyRequired<Person>) {
-    return `${person.name} is ${person.age}`;
-}
-
-const person: Person = {
+//the two properties in the union have to exist in Person, and we are typing to a Person type, but only respecting the name and age props
+const person: MyPick<Person, 'name' | 'age'> = {
     name: 'Todd',
     age: 27
-}
-
-const age = printAge(person);
+};
